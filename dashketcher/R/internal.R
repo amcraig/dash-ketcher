0014c815a145b17ec870afb1bdd10cb53264dec7
Alex Craig <alexander.craig@schrodinger.com>
2023-06-22 16:39:41 -0700

    reset

---
+++ dashketcher/R/internal.R
@@ -0,0 +1,15 @@
+.dashketcher_js_metadata <- function() {
+deps_metadata <- list(`dashketcher` = structure(list(name = "dashketcher",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dashketcher.min.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashketcher",
+all_files = FALSE), class = "html_dependency"),
+`dashketcher` = structure(list(name = "dashketcher",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dashketcher.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashketcher",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
+return(deps_metadata)
+}
